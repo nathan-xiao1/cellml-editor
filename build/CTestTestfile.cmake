25bc42e16177285e58b143d0dab86ad4c023486d
David Leydon <david.leydon@student.unsw.edu.au>
2021-06-23 13:31:56 +1000

    branch for graphical part - not much progress yet

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/admin/Downloads/cellmlthesis/cellml-editor
+# Build directory: C:/Users/admin/Downloads/cellmlthesis/cellml-editor/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
