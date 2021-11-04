e0efda2d492855439d142ec57a0668ae7fcbc742
Mark E <z5206092@cse.unsw.edu.au>
2021-11-04 11:03:47 +1100

    style change

--- src/renderer/components/EquationViewer/ViewOnly-ts.tsx
+++ src/renderer/components/EquationViewer/ViewOnly-ts.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { MathJaxContext, MathJax, MathJax2Config } from 'better-react-mathjax';
 // Import interface which normally isn't public to extend it
 import { MathMLInputProcessor } from 'better-react-mathjax/MathJax2';
+import './styles.css';
 
 // Little hack to get over undefined extensions in original interface declaration
 interface MathMLInputProcessorE extends MathMLInputProcessor {
@@ -24,11 +25,13 @@ export default class ViewOnly extends React.Component<EFProps> {
     render() : React.ReactNode {
         const mathstr = this.props.mathmlstr.replace(/cellml:.\S+\s*=\s*("|')\S*("|')/gm,'');
         return (
-            <MathJaxContext version={2} config={config}> 
-                <MathJax inline={true} dynamic={true}>
-                    <div dangerouslySetInnerHTML={{__html: mathstr}}/>
-                </MathJax>
-            </MathJaxContext>
+            <div className='mathjax'>
+                <MathJaxContext version={2} config={config}> 
+                    <MathJax inline={false} dynamic={true}>
+                        <div dangerouslySetInnerHTML={{__html: mathstr}}/>
+                    </MathJax>
+                </MathJaxContext>
+            </div>
         );
     }
 }
